5bab544c257aef28fc98f4c53a0f42300ab264d7
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2024-08-10 04:48:45 +0200

    updating Css colours

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import devDotToIcon from "../images/socials/devdotto.svg";
+import facebookIcon from "../images/socials/facebook.svg";
+import whatsappIcon from "../images/socials/whatsapp.svg";
 import envelopeIcon from "../images/socials/envelope.svg";
 import gitHubIcon from "../images/socials/github.svg";
 import instagramIcon from "../images/socials/instagram.svg";
